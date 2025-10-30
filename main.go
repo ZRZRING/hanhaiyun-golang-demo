5fb9ee93383f79bb08914aec476959803db5516e
zrzring <zrzring@qq.com>
2025-10-30 16:56:28 +0800

    运行在zrzring.top

--- main.go
+++ main.go
@@ -7,26 +7,26 @@ import (
 	"examination-papers/data/redis"
 	"examination-papers/middleware"
 	"examination-papers/routes"
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 	_ "github.com/joho/godotenv/autoload"
-	"log"
-	"os"
 )
 
 func main() {
-	//app := fiber.New()
+	// app := fiber.New()
 	//
-	//app.Get("/", func(c *fiber.Ctx) error {
+	// app.Get("/", func(c *fiber.Ctx) error {
 	//	return c.SendString("Hello, World!")
-	//})
+	// })
 	//
-	//app.Listen(":3000")
+	// app.Listen(":3000")
 	dbConfig := db.Config{
-		Host:     "localhost",
-		Port:     25432,
-		User:     "postgres",
-		Password: "postgres",
-		DBName:   "postgres",
+		Host:     "zrzring.top",
+		Port:     54321,
+		User:     "unisudo-edu",
+		Password: "unisudo-edu",
+		DBName:   "unisudo-edu",
 		SSLMode:  "disable",
 	}
 	config := configs.FiberConfig()
@@ -37,9 +37,9 @@ func main() {
 		panic(err)
 	}
 	redisConfig := redis.Config{
-		Host:     "localhost",
+		Host:     "zrzring.top",
 		Port:     6379,
-		Password: "eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81",
+		Password: "",
 		DB:       0,
 	}
 	redisClient, err := redis.NewRedisClient(redisConfig)
@@ -54,7 +54,10 @@ func main() {
 		go examCase.SubmitAnswerWorker()
 	}
 	routes.PublicRoutes(app, examCase)
-	log.Printf("Starting server on port %s", os.Getenv("PORT"))
-	port := os.Getenv("PORT")
-	app.Listen(port)
+	serveURL := "127.0.0.1:8080"
+	log.Printf("Starting server on port %s", serveURL)
+	err = app.Listen(serveURL)
+	if err != nil {
+		panic(err)
+	}
 }
